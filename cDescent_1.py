a47389de897aaf8fe78bed2122e255d67bdf60f2
DaasDaham <saad18409@iiitd.ac.in>
2020-12-03 08:57:02 +0530

    lmajk

--- cDescent_1.py
+++ cDescent_1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import copy
 def cDescent(gamma, y, b, lamda, a, zones, newX, x, n1, m1):
-	x=copy.deepcopy(newX)
+	x=np.copy(newX)
 	for i in range(0,m1):
 		for j in range(0,n1):
 			temp = 0
@@ -10,4 +10,3 @@ def cDescent(gamma, y, b, lamda, a, zones, newX, x, n1, m1):
 			temp = (gamma*y[i][j]+b[i][j]-lamda[i][j]-temp+a[i][i]*x[i][j])/(a[i][i]+zones[i][j])
 			x[i][j] = max(temp,0)
 	return x
-	#return np.random.rand(m1,n1)
